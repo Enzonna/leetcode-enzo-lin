25f30e5b34aa432c24689fe0013828c85211181f
Enzonna <1825437118@qq.com>
2024-04-20 16:57:21 +0800

    动态规划

--- src/leetcode/Code005.java
+++ src/leetcode/Code005.java
@@ -1,7 +1,36 @@
-package leetcode;/**
-* Package: leetcode
-* Description: Longest Palindromic Substring - Medium
-* @Author ENZO
-* @Create 2024年4月20日 16:53 
-*/public class Code005 {
+package leetcode;
+
+/**
+ * Package: leetcode
+ * Description: Longest Palindromic Substring - Medium
+ *
+ * @Author ENZO
+ * @Create 2024年4月20日 16:53
+ */
+public class Code005 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+
 }
